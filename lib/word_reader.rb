f1a191103f36f8ab3bd3e8b66b962fd8adf222a0
Timofey Arsenov <arsenov.timofei@yandex.ru>
2016-11-17 00:07:05 +0300

    Update word_reader.rb

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -9,16 +9,14 @@ class WordReader
   # Сохраним старую возможность читать слово из аргументов командной строки.
   # В качестве отедльного метода для обратной совместимости.
   def read_from_args
-    string = ARGV[0]
-    string = UnicodeUtils.upcase(string)
-    string
+    UnicodeUtils.upcase(ARGV[0])
   end
 
   # Метод, возвращающий случайное слово, прочитанное из файла,
   # имя файла передается как аргумент метода
   def read_from_file(file_name)
     # проверка, если файла не существует, сразу возвращаем nil
-    return nil if !File.exist?(file_name)
+    return nil if (File.exist?(file_name)==false)
 
     end
     f = File.new(file_name, "r:UTF-8") # открываем файл, явно указывая его кодировку
